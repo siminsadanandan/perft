5430d7ecfb481c764715948b2ad8d5a3f2475e61
siminsadanandan <44857730+siminsadanandan@users.noreply.github.com>
2023-01-13 21:12:03 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ GITHUB_REPO_USR=""
 
 DOCKER_USERNAME=sisadana
 DOCKER_PASSWORD=
-DOCKER_REPO_NAME="sisadana/mjolnir"
+DOCKER_REPO_NAME="sisadana/gen/mjolnir"
 DOCKER_IMAGE_NAME="perft"
 DOCKER_IMAGE_TAG="1.0"
 echo $DOCKER_PASSWORD | docker login --username $DOCKER_USERNAME --password-stdin
