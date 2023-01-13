f6db1f297e7173a6e7a121afed254c3967b7b157
siminsadanandan <44857730+siminsadanandan@users.noreply.github.com>
2023-01-13 16:59:42 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,14 @@
-#
+#!/bin/bash
+
+## Config
+GITHUB_REPO_NAME="https://github.com/siminsadanandan/perft.git"
+GITHUB_REPO_AUTHKEY=""
+GITHUB_REPO_USR="simin.sadanandan@gmail.com"
+DOCKER_ENDPOINT="https://index.docker.io/v1/"
+DOCKER_REPO_NAME="sisadana/mjolnir"
+DOCKER_IMAGE_NAME="perft"
+DOCKER_IMAGE_TAG="1.0"
+
+
+docker build --no-cache \
+  -t "${DOCKER_REPO_NAME}/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_TAG}" .
