5efdd910864f4ca6e5db623b505088a01ab78c23
siminsadanandan <44857730+siminsadanandan@users.noreply.github.com>
2023-01-13 22:16:21 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
 ## Config
-GITHUB_REPO_NAME="https://github.com/siminsadanandan/perft.git"
+GITHUB_REPO_URL="https://siminsadanandan@github.com/siminsadanandan/perft.git"
 GITHUB_REPO_AUTHKEY=""
-GITHUB_REPO_USR="siminsadanandan"
 
-
-DOCKER_USERNAME=sisadana
-DOCKER_PASSWORD=
-DOCKER_REPO_NAME="sisadana/gen/mjolnir"
+DOCKER_USERNAME="sisadana"
+DOCKER_PASSWORD=""
+DOCKER_REPO_NAME="sisadana"
 DOCKER_IMAGE_NAME="perft"
 DOCKER_IMAGE_TAG="1.0"
+
+##Clone the repo
+git clone ${GITHUB_REPO_URL}
+
+cd perft
+
 echo $DOCKER_PASSWORD | docker login --username $DOCKER_USERNAME --password-stdin
 
 docker build --no-cache \
